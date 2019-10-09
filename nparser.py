9070400de178342c46b3cf299545eff6c0c256b0
Lukasz Korbasiewicz <lukasz.korbasiewicz@gmail.com>
2019-10-09 10:28:33 +0200

    Create nparser.py

---
+++ nparser.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# nparser.py out.xml
+
+import sys
+from libnmap.parser import NmapParser
+
+nmap_report = NmapParser.parse_fromfile(sys.argv[1])
+
+hostport = []
+
+for host in nmap_report.hosts:
+    for service in host.services:
+        if service.state != "open|filtered":
+            hostport.append(str(host.ipv4) + "," + str(service.port))
+
+for item in hostport:
+    print(item)
